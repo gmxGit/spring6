27ff9a8de93c1e89293972d92159afa7574fc9f8
gmxGit <g1998a@outlook.com>
2024-03-14 15:25:29 +0800

    druid dataase connect and practice

---
+++ spring-ioc-xml/src/main/java/Emp.java
@@ -0,0 +1,9 @@
+import lombok.Data;
+
+@Data
+public class Emp {
+    private String id;
+    private String name;
+    private String age;
+    private String sex;
+}
--- spring-ioc-xml/src/test/java/testJdbc.java
+++ spring-ioc-xml/src/test/java/testJdbc.java
@@ -1,13 +1,47 @@
-import com.alibaba.druid.pool.DruidDataSource;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import java.util.List;
+
+@SpringJUnitConfig(locations = "classpath:bean-jdbc.xml")
 public class testJdbc {
- @Test
-    public void testJdbc(){
-     ApplicationContext context=new ClassPathXmlApplicationContext("bean-jdbc.xml");
-     DruidDataSource bean = context.getBean(DruidDataSource.class);
-     System.out.println(bean.getUrl());
- }
+    //    @Test
+//    public void testJdbc() {
+//     ApplicationContext context=new ClassPathXmlApplicationContext("bean-jdbc.xml");
+//        DruidDataSource bean = context.getBean(DruidDataSource.class);
+//        System.out.println(bean.getUrl());
+//    }
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testInsert() {
+        String sql = "insert into t_emp values(null,?,?,?)";
+        jdbcTemplate.update(sql, "小王", "21", "女");
+        jdbcTemplate.update(sql, "lili", "22", "女");
+        jdbcTemplate.update(sql, "liangliang", "23", "男");
+    }
+
+    @Test
+    public void updateFromEmpTest() {
+        String sql = "update  t_emp set name=?,age=? where id=?";
+        jdbcTemplate.update(sql, "小李", "22", "2");
+    }
+
+    @Test
+    public void deleteFromEmpTest() {
+        String sql = "delete from t_emp where id=?";
+        jdbcTemplate.update(sql, 2);
+    }
+
+    @Test
+    public void queryFromEmp() {
+        String sql = "select * from t_emp where id=?";
+        List<Emp> query = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Emp.class), 1);
+        System.out.println(query);
+    }
+
 }
